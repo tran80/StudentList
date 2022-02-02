178c374a3c12729922ab5a82d24599da3f3764aa
tran80 <tran80@sheridancollege.ca>
2022-02-02 12:12:30 -0500

    Added Part-time student

---
+++ src/studentlist1/PartTimeStudent.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentlist1;
+
+/**
+ *
+ * @author tran80
+ */
+public class PartTimeStudent  extends Student {
+    private int numCourses;
+
+    public PartTimeStudent(int numCourses, String name) {
+        super(name);
+        this.numCourses = numCourses;
+    }
+
+    public int getNumCourses() {
+        return numCourses;
+    }
+
+    public void setNumCourses(int numCourses) {
+        this.numCourses = numCourses;
+    }
+    
+}
